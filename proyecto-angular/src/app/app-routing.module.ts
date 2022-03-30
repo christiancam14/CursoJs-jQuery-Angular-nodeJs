2125bd7da32b3d44bc096ce6840274e60b99f9e1
Christian Camacho <chriscamacho1045@gmail.com>
2022-03-30 15:39:46 -0500

    Begin the project

--- proyecto-angular/src/app/app-routing.module.ts
+++ proyecto-angular/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
+const appRoutes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'sobre-mi', component: AboutComponent},
+  {path: 'proyectos', component: ProjectsComponent},
+  {path: 'crear-proyecto', component: CreateComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: '**', component: ErrorComponent},
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
